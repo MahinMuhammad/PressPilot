7856b2d04df1d24dcbea8e3266b1cbd47539f874
MahinMuhammad <amimahinurrahman@gmail.com>
2023-06-17 14:30:53 +0600

    rework on myprofile view to fix UI

--- PressPilot/Views/MyProfileView.swift
+++ PressPilot/Views/MyProfileView.swift
@@ -40,116 +40,153 @@ struct MyProfileView: View {
             ZStack {
                 Color("MyProfileBGColor")
                     .edgesIgnoringSafeArea(.all)
-                VStack{
-                    RoundedRectangle(cornerRadius: 15)
-                        .padding(.leading)
-                        .padding(.trailing)
-                        .foregroundColor(Color("ProfileInfoColor"))
-                        .frame(height: 330)
-                        .overlay{
-                            VStack{
-                                RoundedRectangle(cornerRadius: 50)
-                                    .frame(width: 150, height: 150)
-                                    .foregroundColor(Color.gray)
-                                    .overlay{
-                                        Image(systemName: "figure.stand")
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(height: 100)
-                                    }
-                                
-                                Text("\(dataService.userData?.firstName ?? "") \(dataService.userData?.lastname ?? "")")
-                                    .bold()
-                                    .font(.system(size: 23))
-                                    .padding(.top)
-                                    .padding(.bottom, 1)
-                                
-                                Text(dataService.userData?.email ?? "")
-                                    .bold()
-                                    .font(.system(size: 15))
-                                    .tint(Color(UIColor.darkGray))
-                                
-                                Button{
-                                    
-                                }label: {
+                ScrollView {
+                    VStack(spacing: 20){
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundColor(Color("ProfileInfoColor"))
+                            .frame(height: 330)
+                            .overlay{
+                                VStack{
                                     RoundedRectangle(cornerRadius: 50)
-                                        .frame(width: 159, height: 40)
+                                        .frame(width: 150, height: 150)
+                                        .foregroundColor(Color.gray)
                                         .overlay{
-                                            HStack{
-                                                Text("Edit Profile")
-                                                Image(systemName: "chevron.forward")
-                                            }
-                                            .fontWeight(.medium)
-                                            .foregroundColor(.white)
+                                            Image(systemName: "figure.stand")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(height: 100)
                                         }
+                                    
+                                    Text("\(dataService.userData?.firstName ?? "") \(dataService.userData?.lastname ?? "")")
+                                        .bold()
+                                        .font(.system(size: 23))
+                                        .padding(.top)
+                                        .padding(.bottom, 1)
+                                    
+                                    Text(dataService.userData?.email ?? "")
+                                        .bold()
+                                        .font(.system(size: 15))
+                                        .tint(Color(UIColor.darkGray))
+                                    
+                                    Button{
+                                        
+                                    }label: {
+                                        RoundedRectangle(cornerRadius: 50)
+                                            .frame(width: 159, height: 40)
+                                            .overlay{
+                                                HStack{
+                                                    Text("Edit Profile")
+                                                    Image(systemName: "chevron.forward")
+                                                }
+                                                .fontWeight(.medium)
+                                                .foregroundColor(.white)
+                                            }
+                                    }
                                 }
                             }
-                        }
-                    
-                    List {
-                        Picker(selection: $networkManager.rs.selectedLanguage, label: PickerLabelView(imageName: "doc.plaintext", title: "Language")) {
-                            ForEach(networkManager.rs.languages) { language in
-                                Text(language.language).tag(language.id)
-                            }
-                        }
-                        .foregroundColor(Color(UIColor.label))
-                        .pickerStyle(.navigationLink)
                         
-                        Picker(selection: $networkManager.rs.selectedCountry, label: PickerLabelView(imageName: "globe", title: "Country")) {
-                            ForEach(networkManager.rs.countries) { country in
-                                Text(country.country).tag(country.id)
+                        RoundedRectangle(cornerRadius: 25)
+                            .frame(height: 110)
+                            .foregroundColor(Color(UIColor.systemBackground))
+                            .overlay{
+                                VStack{
+                                    HStack{
+                                        Picker(selection: $networkManager.rs.selectedLanguage, label: PickerLabelView(imageName: "doc.plaintext", title: "Language")) {
+                                            ForEach(networkManager.rs.languages) { language in
+                                                Text(language.language).tag(language.id)
+                                            }
+                                        }
+                                        .foregroundColor(Color(UIColor.label))
+                                        .pickerStyle(.navigationLink)
+                                        
+                                        Image(systemName: "chevron.forward")
+                                            .foregroundColor(Color(UIColor.lightGray))
+                                    }
+                                    
+                                    Divider()
+                                    
+                                    HStack{
+                                        Picker(selection: $networkManager.rs.selectedCountry, label: PickerLabelView(imageName: "globe", title: "Country"))
+                                        {
+                                            ForEach(networkManager.rs.countries) { country in
+                                                Text(country.country).tag(country.id)
+                                            }
+                                        }
+                                        .foregroundColor(Color(UIColor.label))
+                                        .pickerStyle(.navigationLink)
+                                        
+                                        Image(systemName: "chevron.forward")
+                                            .foregroundColor(Color(UIColor.lightGray))
+                                    }
+                                }
+                                .padding()
                             }
-                        }
-                        .foregroundColor(Color(UIColor.label))
-                        .pickerStyle(.navigationLink)
-                    }
-                    .scrollDisabled(true)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                    
-                    List{
-                        Button{
-                            showRemoveAllNewsAlert = true
-                        }label: {
-                            HStack{
-                                Image(systemName: "bookmark.slash")
-                                Text("Remove saved news")
-                                Spacer()
-                                Image(systemName: "chevron.forward")
-                                    .foregroundColor(Color(UIColor.lightGray))
-                                    .imageScale(.small)
-                            }
-                        }
-                        .tint(Color(UIColor.label))
-                        .alert("Remove all saved news", isPresented: $showRemoveAllNewsAlert) {
-                            Button("No", role: .cancel) { }
-                            Button("Yes", role: .destructive) {
-                                dataService.deleteAllSaveNews()
-                                let impactMed = UIImpactFeedbackGenerator(style: .heavy)
-                                impactMed.impactOccurred()
-                            }
-                        }
                         
-                        HStack{
-                            Image(systemName: "rectangle.and.arrow.up.right.and.arrow.down.left.slash")
-                            Text("Delete downloaded news")
-                            Spacer()
-                            Image(systemName: "chevron.forward")
-                                .foregroundColor(Color(UIColor.lightGray))
-                                .imageScale(.small)
-                        }
+                        RoundedRectangle(cornerRadius: 25)
+                            .frame(height: 110)
+                            .foregroundColor(Color(UIColor.systemBackground))
+                            .overlay{
+                                VStack{
+                                    Button{
+                                        showRemoveAllNewsAlert = true
+                                    }label: {
+                                        HStack{
+                                            Image(systemName: "bookmark.slash")
+                                            Text("Remove saved news")
+                                            Spacer()
+                                            Image(systemName: "chevron.forward")
+                                                .foregroundColor(Color(UIColor.lightGray))
+                                        }
+                                    }
+                                    .tint(Color(UIColor.label))
+                                    .alert("Remove all saved news", isPresented: $showRemoveAllNewsAlert) {
+                                        Button("No", role: .cancel) { }
+                                        Button("Yes", role: .destructive) {
+                                            dataService.deleteAllSaveNews()
+                                            let impactMed = UIImpactFeedbackGenerator(style: .heavy)
+                                            impactMed.impactOccurred()
+                                        }
+                                    }
+                                    
+                                    Divider()
+                                    
+                                    HStack{
+                                        Image(systemName: "rectangle.and.arrow.up.right.and.arrow.down.left.slash")
+                                        Text("Delete downloaded news")
+                                        Spacer()
+                                        Image(systemName: "chevron.forward")
+                                            .foregroundColor(Color(UIColor.lightGray))
+                                    }
+                                }
+                                .padding()
+                            }
+                        
+                        RoundedRectangle(cornerRadius: 25)
+                            .frame(height: 80)
+                            .foregroundColor(Color(UIColor.systemBackground))
+                            .overlay{
+                                Button{
+                                    logoutSuccess = authService.signOut()
+                                }label: {
+                                    Text("Log Out")
+                                        .fontWeight(.medium)
+                                        .font(.system(size: 22))
+                                        .tint(Color(UIColor.label))
+                                        .frame(minWidth: 0, maxWidth: .infinity)
+                                        .frame(height: 55)
+                                        .overlay{
+                                            RoundedRectangle(cornerRadius: 20)
+                                                .stroke(Color(UIColor.label), lineWidth: 1)
+                                        }
+                                        .padding(10)
+                                }
+                                .navigationDestination(isPresented: $logoutSuccess) {
+                                    SignInView()
+                                }
+                            }
                     }
-                    .scrollDisabled(true)
-                    .padding(.top,-40)
-                    
-                    Spacer()
-                }
-                .toolbar{
-                    Button("Log Out"){
-                        logoutSuccess = authService.signOut()
-                    }
-                    .navigationDestination(isPresented: $logoutSuccess) {
-                        SignInView()
-                    }
+                    .padding(.leading)
+                    .padding(.trailing)
                 }
                 if dataService.userData == nil{
                     ZStack{
